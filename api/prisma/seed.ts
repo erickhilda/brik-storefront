ae7aeaa76dfc8d5ae4c59f2ee0f1aa174b60ce9d
erickhilda <erickh.andreas@gmail.com>
2023-11-15 13:27:57 +0700

    add seeding data script

--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -1,9 +1,35 @@
 import { PrismaClient, Prisma } from "@prisma/client";
+import seedData from "./seed-data.json";
+console.log("🚀 ~ seedData:", seedData);
 
 const prisma = new PrismaClient();
 
 async function main() {
   console.log(`Start seeding ...`);
+  const { product, productCategory } = seedData;
+
+  for (const category of productCategory) {
+    const { name, path } = category;
+    await prisma.product_Category.create({
+      data: {
+        name,
+        path,
+      },
+    });
+  }
+
+  for (const item of product) {
+    const { name, description, price, category_id, image_url } = item;
+    await prisma.product.create({
+      data: {
+        name,
+        description,
+        price,
+        category_id,
+        image_url,
+      },
+    });
+  }
   console.log(`Seeding finished.`);
 }
 
